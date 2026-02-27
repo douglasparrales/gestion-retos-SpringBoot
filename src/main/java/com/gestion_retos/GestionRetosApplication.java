07968b68c63ab2e7ca35e35cad50fed252748712
douglasparrales <douglas.app.26@gmail.com>
2026-02-26 22:33:37 -0500

    first commit

---
+++ src/main/java/com/gestion_retos/GestionRetosApplication.java
@@ -0,0 +1,13 @@
+package com.gestion_retos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionRetosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionRetosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gestion_retos/GestionRetosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gestion_retos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionRetosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
