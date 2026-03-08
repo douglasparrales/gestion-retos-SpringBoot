ac418f08e880499d499a547ae4120a75329d0ca3
douglasparrales <douglas.app.26@gmail.com>
2026-03-08 02:05:46 -0500

    global exception

---
+++ src/main/java/com/gestion_retos/exception/IllegalStateException.java
@@ -0,0 +1,7 @@
+package com.gestion_retos.exception;
+
+public class IllegalStateException extends RuntimeException{
+    public IllegalStateException(String message){
+        super(message);
+    }
+}
