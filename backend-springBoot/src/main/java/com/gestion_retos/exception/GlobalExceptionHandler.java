0d5b4b7e46b8635d817702d1fe1c2ae2e618a7d2
douglasparrales <douglas.app.26@gmail.com>
2026-03-29 23:39:14 -0500

    chore

--- backend-springBoot/src/main/java/com/gestion_retos/exception/GlobalExceptionHandler.java
+++ backend-springBoot/src/main/java/com/gestion_retos/exception/GlobalExceptionHandler.java
@@ -1,7 +1,6 @@
 package com.gestion_retos.exception;
 
 import com.gestion_retos.payload.ErrorResponse;
-import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
